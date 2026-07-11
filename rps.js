2e569757e42fe18e2e5cd5780eab2f6f288e1f28
jaimef7 <jaimef@junior.localdomain>
2026-07-11 01:57:21 -0500

    added the game function

--- rps.js
+++ rps.js
@@ -1,7 +1,5 @@
 let getHumanScore = 0;
 let GetComputerscore = 0;
-const humanChoice =getHumanChoice();
-const computerChoice =getComputerChoice();
 
 
 function getComputerChoice() {
@@ -37,12 +35,34 @@ function getComputerChoice() {
         return "its a tie!";
     } else if ((humanChoice ==="rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice ==="rock") || (humanChoice ==="scissors" && computerChoice === "paper")){
         getHumanScore++ ;
-        return "Yay you win" 
+        return `Yay you win ${humanChoice} beats ${computerChoice}`;
     }else {
         GetComputerscore++;
-        return "oh no Computer Wins";
+        return `oh no Computer Wins ${computerChoice} beats ${humanChoice}`;
     }
 
   }
-  console.log(playRound( humanChoice, computerChoice));
+
+  function playGame(){
+
+    for(let i=0; i<=4;i++){
+        const humanChoice =getHumanChoice();
+        const computerChoice =getComputerChoice();
+
+        const result = playRound(humanChoice,computerChoice);
+        console.log(result);
+    }
+    console.log(`Human: ${getHumanScore}`);
+    console.log(`Computer: ${GetComputerscore}`);
+
+    if(GetComputerscore > getHumanScore){
+        return "you lost!";
+    }else if(GetComputerscore < getHumanScore){
+        return "you won!";
+    }else{
+        return "you lost";
+    }
+
+  }
+  console.log(playGame());
 
